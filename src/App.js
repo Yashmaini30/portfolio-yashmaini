0d09cec42b29cc0c287823e2284835793c496075
Yash Maini <mainiyash2@gmail.com>
2024-11-11 14:19:29 +0530

    made navbar styled it and tested it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './componennts/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Navbar/>
   );
 }
 
