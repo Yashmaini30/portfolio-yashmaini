154c22f84661be71de7ab028ecd5019ff01fb32d
Yash Maini <mainiyash2@gmail.com>
2024-11-15 21:38:08 +0530

    designed and tested home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './componennts/Navbar';
+import Home from './componennts/Home';
 
 
 function App() {
   return (
+    <>
     <Navbar/>
+    <Home/>
+    </>  
   );
 }
 
---
+++ src/componennts/Home.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Lottie from  "lottie-react";
+import SpaceBoy from "../LottieFiles/SpaceBoy.json";
+import Typed from "./Typed.jsx";
+import Tilt from 'react-parallax-tilt';
+import Avatar from '../images/Avatar.png';
+import {CiCoffeeCup} from "react-icons/ci";
+
+const Home = () => {
+  return (
+    <div >
+      <div className='HomePage'>
+
+        <div className='HomeText'>
+          <h1>Let's connect!</h1>
+          <h1>I'M <b>YASH MAINI</b></h1>
+          <Typed/>   
+        </div>
+
+        <Lottie 
+          className="illustration" 
+          animationData={SpaceBoy} 
+          loop={true} 
+        />
+        
+      </div>
+
+      <div className='AboutPage'>
+        <div className='AboutText'>
+          <h1 className='AboutTextHeading'>Brief <b>introduction</b></h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum neque reprehenderit aspernatur, maiores unde voluptate ipsam nostrum et eius dignissimos illo voluptates recusandae cupiditate totam dolores vero ducimus perferendis magni?<br /><br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, tenetur! <b>sdfg</b> <br /><br />
+            Also, I love <b>coffee</b> <CiCoffeeCup style={{scale:"1.5", rotate:"15deg"}}/>   
+          </p>
+        </div>
+        <Tilt>
+          <img className='Avatar' src={Avatar} alt="" />
+        </Tilt>
+      </div>
+    </div>
+  )
+}
+
+export default Home
