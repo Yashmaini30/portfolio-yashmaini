199d55de09d6dfa9beacf3c328208056ad816146
Yash Maini <mainiyash2@gmail.com>
2024-11-16 10:15:28 +0530

    made about page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './componennts/Navbar';
 import Home from './componennts/Home';
+import About from './componennts/About';
 
 
 function App() {
   return (
     <>
     <Navbar/>
-    <Home/>
+    <About/>
     </>  
   );
 }
---
+++ src/componennts/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import Lottie from "lottie-react";
+import Coder from "../LottieFiles/coder.json";
+
+const About = () => {
+    return (
+        <>
+        <div className="AboutPage">
+            <div className="AboutText">
+                <h1 className="AboutTextHeading">Get to <b>know</b> me!</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum neque
+                    reprehenderit aspernatur, maiores unde voluptate ipsam nostrum et
+                    eius dignissimos illo <b>hello</b>voluptates recusandae cupiditate totam dolores
+                    vero ducimus perferendis magni? <br />
+                    <br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
+                    tenetur! sdfg
+                </p>
+            </div>
+
+            <div>
+                <Tilt>
+                    <Lottie
+                        className="illustration"
+                        animationData={Coder}
+                        loop={true}
+                    />
+                </Tilt>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default About;
