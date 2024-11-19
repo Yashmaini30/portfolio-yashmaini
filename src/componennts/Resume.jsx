58c1e49fde2d69393801dc482f1f825ad4bf2c72
Yash Maini <mainiyash2@gmail.com>
2024-11-19 19:20:33 +0530

    solved bug(getting preview of 1st page)

--- src/componennts/Resume.jsx
+++ src/componennts/Resume.jsx
@@ -4,26 +4,35 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 const Resume = () => {
-    const [wid, setwid] = useState(window.innerWidth);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [numPages, setNumPages] = useState(null);
 
     useEffect(() => {
         const handleResize = () => {
-            setwid(window.innerWidth);
+            setWindowWidth(window.innerWidth);
         };
 
         window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
+        return () => window.removeEventListener("resize", handleResize);
     }, []);
+    const onDocumentLoadSuccess = (pdf) => {
+        console.log(`PDF loaded with ${pdf.numPages} pages`);
+        setNumPages(pdf.numPages);
+    };
 
     return (
         <div className="ResumePage">
-            <Document file="/Testing.pdf" className="resumeview">
-                <Page pageNumber={1} scale={wid < 700 ? (wid > 475 ? 0.7 : 0.5) : 1} />
+            <Document
+                file="/Testing.pdf"
+                onLoadError={(error) => console.error("Error loading PDF:", error)}
+            >
+                <Page
+                    pageNumber={1} 
+                    scale={windowWidth < 700 ? (windowWidth > 475 ? 0.7 : 0.5) : 1}
+                />
             </Document>
 
             <a href="/Testing.pdf" target="_blank" download="YashMaini_Resume.pdf">
