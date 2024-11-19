5a3d4b1dc71e93c9e230135ba5252fdccc64bbd4
Yash Maini <mainiyash2@gmail.com>
2024-11-19 19:38:26 +0530

    made nav links functional

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import './App.css';
 import Navbar from './componennts/Navbar';
 import Home from './componennts/Home';
 import About from './componennts/About';
 import Footer from './componennts/Footer';
 import Resume from './componennts/Resume';
+import Project from './componennts/Project';
+import Lottie from  "lottie-react";
+import nightsky from "./LottieFiles/night-sky.json";
+import HashLoader from "react-spinners/HashLoader";
 
 
 function App() {
+  const[Loading,SetLoading]=useState(true);
+
+  useEffect(()=>{
+    SetLoading(true)
+
+    setTimeout(()=>{
+    SetLoading(false)}
+    ,1900)
+  },[])  
   return (
     <>
-    <Navbar/>
-    <Resume/>
-    <Footer/>
-    </>  
+      {Loading ? (
+      <div className="loader"> 
+        <HashLoader
+          color={'#9067C6'}
+          loading={true}
+          size={100}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
+      ):(
+      <div>
+      
+      <Lottie className="bg" animationData={nightsky} loop={true} />  
+      <Lottie className="bgtwo" animationData={nightsky} loop={true} />   
+      <Lottie className="bgtemp" animationData={nightsky} loop={true} /> 
+
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Project" element={<Project/>}/>
+        <Route path="/Resume" element={<Resume/>}/>
+      </Routes>
+      
+      <Footer/>
+      </div>
+      )}  
+    </>
   );
 }
 
