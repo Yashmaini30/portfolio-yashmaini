153500dfaad40777ef563ba3b210c6378d47e50d
Yash Maini <mainiyash2@gmail.com>
2025-01-01 19:56:07 +0530

    changed year happy new year

--- src/componennts/Footer.jsx
+++ src/componennts/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer>
       <h4>Developed by Yash Maini</h4>
-      <h4>Copyright &copy; 2024 YM</h4>
+      <h4>Copyright &copy; 2025 YM</h4>
       <div className='footerLinks'>
         <a href="https://github.com/Yashmaini30" target='_blank' rel='noreferrer'><FaGithub/></a>
         <a href="https://www.linkedin.com/in/yash-maini-369869198/" target='_blank' rel='noreferrer'><FaLinkedin/></a>
